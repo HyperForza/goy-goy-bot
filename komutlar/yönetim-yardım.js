5fa6bfd7a992b1f7704fbee3a41937a3bce8ca59
HyperForza <67697041+HyperForza@users.noreply.github.com>
2021-06-06 17:47:47 +0300

    Update yönetim-yardım.js

--- komutlar/yönetim-yardım.js
+++ komutlar/yönetim-yardım.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => { 
-let prefix = '!'
+let prefix = '.'
 let müzik = new Discord.RichEmbed()  
 .setAuthor(message.author.username, message.author.avatarURL)
 .setColor('RANDOM')
@@ -20,28 +19,4 @@ exports.conf = {
 exports.help = {
   name: 'yönetim-yardım'
 };
-  
-=======
-const Discord = require('discord.js');
-exports.run = async (client, message, args) => { 
-let prefix = '!'
-let müzik = new Discord.RichEmbed()  
-.setAuthor(message.author.username, message.author.avatarURL)
-.setColor('RANDOM')
-.addField('Goy Goy Bot | Mod Komutları',`
-**.otorol** : Sunucuya Otorol Ayarlar`)
-
-.setFooter('Goy Goy Bot')
- message.channel.send(müzik) 
-  };
-exports.conf = {
-  enabled: true,  
-  guildOnly: false, 
-  aliases: ["yönetim","yönetim-yardım","yönetim-help"], 
-  permLevel: 0
-};
-exports.help = {
-  name: 'yönetim-yardım'
-};
-  
->>>>>>> 796bf071f57797d3f639ba8fde78b83dc5423972
+ 
