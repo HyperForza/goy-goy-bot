6fe8f7cbd312fa108e6852d932200bba40614b09
HyperForza <67697041+HyperForza@users.noreply.github.com>
2021-06-07 15:42:53 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,21 +2,18 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
-
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-var oyun = [
-        "Goy Goy Bot",
-    ];
+ setInterval(function() {
+}, 5000);
+client.user.setPresence({
+        game: {
+            name: `Goy Goy BOT`,
+            type: 'LISTINING'   //WATCHING = İZLİYOR        LISTINING = DİNLİYOR
+        },
+        status: 'online'   //online - Çevrimiçi           idle - Boşta          dnd - Rahatsız Etmeyin        ofline - Görünmez
+    })
+    console.log(`Bot Hazır, Komutlar Yüklendi`);
+}
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-    
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  client.user.setActivity(`.yardım`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-};
