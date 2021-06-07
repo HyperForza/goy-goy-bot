b8f38693bb71bca3db7c4347189b0eedef15013a
HyperForza <67697041+HyperForza@users.noreply.github.com>
2021-06-07 15:36:10 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,9 +13,6 @@ var oyun = [
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "" );
-        }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
